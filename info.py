f103c97f68729063aad85f394a2813562b82f617
Tamadaddi1 <shareeftamadaddi1@gmail.com>
2024-11-06 17:37:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,7 +52,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "  mongodb+srv://shareeftamadaddi1:F0InumvPZwkoHTqz@cluster0.8x5qv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
+DATABASE_NAME = environ.get('DATABASE_NAME', "shareeftamadaddi1")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 
 # Premium And Referal Settings
